12f4e2cbf8a2b4752ba4923f50d567639b4c65ee
stat-opole <66861258+stat-opole@users.noreply.github.com>
2020-06-29 09:07:10 +0200

    Drobna zmiana

--- scripts/game_3.js
+++ scripts/game_3.js
@@ -68,7 +68,7 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 
-today = mm + '.' + dd + '.' + yyyy;
+today = dd + '.' + mm + '.' + yyyy;
 
 document.getElementById('txt2').innerHTML =
   today;
