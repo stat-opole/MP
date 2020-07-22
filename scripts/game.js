9bc86697032d55b2ef5e4d30767213b0ad57cd55
stat-opole <66861258+stat-opole@users.noreply.github.com>
2020-07-22 13:35:27 +0200

    Add files via upload

--- scripts/game.js
+++ scripts/game.js
@@ -1,107 +1,65 @@
  const puzzle_level = 4;
  var canvas, ctx;
- 
+
  var ctx, startX, startY;
 
- var pieces, dragHoldX, dragHoldY;
+ var pieces;
  var puzzleWidth;
  var puzzleHeight;
  var pieceWidth;
  var pieceHeight;
-
  var piecePuzzle;
 
- var drag=false;
  var mouseX;
  var mouseY;
  var currentPiece;
  var currentDropPiece;
+ var touchSupported=false;
 
- 
  var dd,mm,yyyy;
- 
- var mx, my;
  var start, seconds, minutes, hours, t;
  	
-start = document.getElementById('txt3'),
+ start = document.getElementById('txt3'),
    
  seconds = 0, minutes = 0, hours = 0, t;
-    start.textContent = "00:00:00";
+ start.textContent = "00:00:00";
 
- 
- var touchSupported=false;
- 
-
- 
- function init()
- {
-	 
-	 canvas = document.getElementById("myCanvas"),
-	 ctx = canvas.getContext("2d");
-	 mouse = {x:0,y:0};
-	 img = new Image();
-	 img.addEventListener('load',onImage,false);
-	 img.src = "images/Puzzle4.JPG";
-	 
-	 //start_game = document.getElementById('start'),
-	 // img2=new Image();
-	  // img2.addEventListener('load',onImage,false);
-	 // img2.src="images/start.jpg";
-
+ function init(){	 
+	canvas = document.getElementById("myCanvas"),
+	ctx = canvas.getContext("2d");
+	mouse = {x:0,y:0};
+	img = new Image();
+	img.addEventListener('load',onImage,false);
+	img.src = "images/Puzzle4.JPG";
  }
- 
-  // if (Modernizr.touch) {
 
-        // touchSupported = true;
-
-        // eventsMap  = {
-
-            // select: "touchstart",
-
-            // down: "touchstart",
-
-            // up: "touchend",
-
-            // move: "touchmove"
-
-        // };
-
-    // }
- 
  function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =
-  h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
-  dateTime();
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('txt').innerHTML =
+	h + ":" + m + ":" + s;
+	var t = setTimeout(startTime, 500);
+	dateTime();
 }
 function checkTime(i) {
-  if (i < 10) {i = "0" + i}; 
-  return i;
+	if (i < 10) {i = "0" + i}; 
+	return i;
 }
 
 function dateTime() {
-		var today = new Date();
-dd = String(today.getDate()).padStart(2, '0');
-mm = String(today.getMonth() + 1).padStart(2, '0');
-yyyy = today.getFullYear();
+	var today = new Date();
+	dd = String(today.getDate()).padStart(2, '0');
+	mm = String(today.getMonth() + 1).padStart(2, '0');
+	yyyy = today.getFullYear();
 
-today = dd + '.' + mm + '.' + yyyy;
+	today = dd + '.' + mm + '.' + yyyy;
 
-document.getElementById('txt2').innerHTML =
-  today;
-  
-
- 
+	document.getElementById('txt2').innerHTML = today;
 }
-
-
-    
 
 function add() {
 	
@@ -119,74 +77,47 @@ function add() {
 
     timer();
 }
-function timer() {
-	
-	
+function timer() {		
     t = setTimeout(add, 1000);
 }
 
 
- function onImage()
- {
-	 
+ function onImage(){	 
 	pieceWidth = Math.floor(img.width / puzzle_level);
     pieceHeight = Math.floor(img.height / puzzle_level);
     
 	puzzleWidth = pieceWidth * puzzle_level;
     puzzleHeight = pieceHeight * puzzle_level;
     startTime();
-//timer();
 	setCanvas();
-    //initPuzzle();
 }
 
-function setCanvas()
-{
+function setCanvas(){
     canvas.width = puzzleWidth;
     canvas.height = puzzleHeight;
     canvas.style.border = "1px solid black";
 }
 
-function initPuzzle()
-{
+function initPuzzle(){
 	currentPiece = null;
     currentDropPiece = null
 	timer();
 	document.getElementById("start").style.display = "none"; 
 	
 	if (Modernizr.touch) {
-
         touchSupported = true;
-
-        // eventsMap  = {
-
-            // select: "touchstart",
-
-            // down: "touchstart",
-
-            // up: "touchend",
-
-            // move: "touchmove"
-
-        // };
-
     }
 	
     buildPieces();
 }
-
-
-
 	
-function buildPieces()
-{
+function buildPieces(){
 	pieces=[];
 	var piece;
 	var wx=0;
 	var wy=0;
 	
-	for(var i=0; i<puzzle_level*puzzle_level;i++)
-	{
+	for(var i=0; i<puzzle_level*puzzle_level;i++){
 		piece={};
 		piece.x=wx;
 		piece.y=wy;
@@ -196,33 +127,23 @@ function buildPieces()
 		piece.yy=0;
 		
 		pieces.push(piece);
-		
 		wx+=pieceWidth;
 		
-		
-		if(wx>=puzzleWidth)
-		{
+		if(wx>=puzzleWidth){
 			wx=0;
 			wy+=pieceHeight;
-		}
-		
-		
-		
-		
+		}	
 	}
 	
-	shufflePuzzle();
-
-	
+	shufflePuzzle();	
 }
 
 function shufflePuzzle(){
     pieces = shuffleArray(pieces);
-    var i;
     var piece;
     var wx = 0;
     var wy = 0;
-    for(i = 0;i < pieces.length;i++){
+    for(var i = 0;i < pieces.length;i++){
         piece = pieces[i];
         piece.xx = wx;
         piece.yy = wy;
@@ -237,144 +158,96 @@ function shufflePuzzle(){
         }
     }
 	
-	if(!touchSupported)
-	{
+	if(!touchSupported){
 		 document.onmousedown = mouseDown;
 	}
-	else
-	{
-		
-		//img.addEventListener('load',onImage,false);
+	else{
 		document.ontouchstart = mouseDown;
-			
-			//mouseDown();
-		//};
 	}
-    //document.onmousedown = mouseDown;
 }
 
-	
-function shuffleArray(o)
-{
-	for(var i = 0; i<o.length;i++)
-	{
+function shuffleArray(o){
+	for(var i = 0; i<o.length;i++){
 		const j = Math.floor(Math.random()*i);
 		const temp = o[i];
 		o[i]=pieces[j];
 		o[j]=temp;
-	
 	}
     return o;
 }
 
-function inPiece()
-{
+function inPiece(){
 	var piece;
 	
-	for(var i=0; i<pieces.length;i++)
-	{
-		
+	for(var i=0; i<pieces.length;i++){		
 		piece=pieces[i];
-		if(mouseX<piece.xx || mouseX>(piece.xx+pieceWidth)||mouseY<piece.yy || mouseY>(piece.yy+pieceHeight))
-		{
-		
-		}
-		else
-		{
+		if(mouseX<piece.xx || mouseX>(piece.xx+pieceWidth)||mouseY<piece.yy || mouseY>(piece.yy+pieceHeight)){}
+		else{
 			return piece;
-		}
-		
+		}	
 	}
-
+	
 	return null
 }
 
 function mouseDown(e)
 {
-
 	var bRect = canvas.getBoundingClientRect();
-	if(!Modernizr.touch)
-	{
+	if(!Modernizr.touch){
 		mouseX = (e.clientX - bRect.left);
-	mouseY = (e.clientY - bRect.top);
+		mouseY = (e.clientY - bRect.top);
 	}
-	else
-	{
-	
+	else{
 		mouseX = (e.touches[0].clientX - bRect.left);
-	mouseY = (e.touches[0].clientY - bRect.top);
+		mouseY = (e.touches[0].clientY - bRect.top);
 	}
-	
-
-
-	
-	
-	
 	
 	currentPiece=inPiece();
 	
-	if(currentPiece!=null)
-	{
+	if(currentPiece!=null){
 		ctx.clearRect(currentPiece.xx,currentPiece.yy,pieceWidth,pieceHeight);
         ctx.save();
         ctx.globalAlpha = .9;
         ctx.drawImage(img, currentPiece.x, currentPiece.y, pieceWidth, pieceHeight, mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth, pieceHeight);
         ctx.restore();
 	
-		if(!touchSupported)
-		{
-			
+		if(!touchSupported){	
 			document.onmousemove = mouseMove;
-        document.onmouseup = mouseUp;
+			document.onmouseup = mouseUp;
 		}
-		else
-		{
-			
+		else{		
 			canvas.addEventListener('touchmove', mouseMove, {passive: false});
 			e.stopPropagation();
-			//document.ontouchmove =mouseMove;
-			//document.ontouchend = mouseUp;
+			
 			document.ontouchend = mouseUp;
-		}
-        
+		}     
 	}
-	
 }
-
-
-
 
 function mouseMove(e)
 {
-	
 	currentDropPiece = null;
-e.preventDefault();
+	e.preventDefault();
 	e.stopPropagation();
 
 	var bRect = canvas.getBoundingClientRect();
 	
-	if(!Modernizr.touch)
-	{
+	if(!Modernizr.touch){
 		mouseX = (e.clientX - bRect.left);
-	mouseY = (e.clientY - bRect.top);
+		mouseY = (e.clientY - bRect.top);
 	}
-	else
-	{
-	
+	else{	
 		mouseX = (e.touches[0].clientX - bRect.left);
-	mouseY = (e.touches[0].clientY - bRect.top);
+		mouseY = (e.touches[0].clientY - bRect.top);
 	}
-	
-	
+		
 	ctx.clearRect(0,0,puzzleWidth,puzzleHeight);
 	
 	var piece;
-	for(var i=0; i<pieces.length;i++)
-	{
+	for(var i=0; i<pieces.length;i++){
 		
 		piece=pieces[i];
-		if(piece==currentPiece)
-		{
+		if(piece==currentPiece){
 			continue;
 		}
 		
@@ -391,39 +264,19 @@ e.preventDefault();
                 ctx.fillRect(currentDropPiece.xx,currentDropPiece.yy,pieceWidth, pieceHeight);
                 ctx.restore();
             }
-        }
-			
+        }		
 	}
 	ctx.save();
     ctx.globalAlpha = .6;
     ctx.drawImage(img, currentPiece.x, currentPiece.y, pieceWidth, pieceHeight, mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth, pieceHeight);
     ctx.restore();
     ctx.strokeRect( mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth,pieceHeight);
-	
 }
 
-
-
-function mouseUp(e)
-{
-	
-	if(!touchSupported)
-	{
-			
-	document.onmousemove = null;
-    document.onmouseup = null;
-	}
-	else
-	{
-		
-		//document.ontouchmove = null;
-		
-				
-				//document.ontouchend = null;
-		
-		
-		
-		//canvas.unbind('touchend');
+function mouseUp(e){	
+	if(!touchSupported){		
+		document.onmousemove = null;
+		document.onmouseup = null;
 	}
 
     if(currentDropPiece != null){
@@ -440,32 +293,25 @@ function mouseUp(e)
 function resetPuzzleAndCheckWin(){
     ctx.clearRect(0,0,puzzleWidth,puzzleHeight);
     var gameWin = true;
-    var i;
     var piece;
-    for(i = 0;i < pieces.length;i++){
+    for(var i = 0;i < pieces.length;i++){
         piece = pieces[i];
 	
         ctx.drawImage(img, piece.x, piece.y, pieceWidth, pieceHeight, piece.xx, piece.yy, pieceWidth, pieceHeight);
         ctx.strokeRect(piece.xx, piece.yy, pieceWidth,pieceHeight);
-       if(piece.xx != piece.x || piece.yy != piece.y){
-            gameWin = false;
-			
-        }
-		
+		if(piece.xx != piece.x || piece.yy != piece.y){
+            gameWin = false;		
+        }		
     }
 	
-	if(gameWin)
-	{
-		
+	if(gameWin){		
 		setTimeout(gameOver,500);
-	}
-    
+	}  
 }
 
 function myStopFunction() {
-              clearInterval(t);
-      }
- 
+    clearInterval(t);
+}
  
 function gameOver(){
     document.onmousedown = null;
@@ -473,22 +319,16 @@ function gameOver(){
     document.onmouseup = null;
 	
 	var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =
-  h + ":" + m + ":" + s;
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('txt').innerHTML =
+	h + ":" + m + ":" + s;
 
 	alert("Gratulacje! \nUkończyłaś/eś poziom łatwy w czasie: "+start.textContent +"\nPrzechodzisz do kolejnego poziomu");
-   myStopFunction();
+	myStopFunction();
 
    window.location.href = "choose_level3.html";
 }
-
-
-
-  
-
-
--- scripts/game_3.js
+++ scripts/game_3.js
@@ -1,59 +1,46 @@
  const puzzle_level = 6;
  var canvas = document.getElementById("myCanvas"), ctx = canvas.getContext("2d");
  
- var ctx, startX, startY;
- var img, img2, img3;
- var pieces, dragHoldX, dragHoldY;
+ var img, img2;
+ var pieces;
  var puzzleWidth;
  var puzzleHeight;
  var pieceWidth;
  var pieceHeight;
-var dd,mm,yyyy;
-var x ;
-
- var touchSupported=false;
- 
- var start, seconds, minutes, hours, t;
- 	
-start = document.getElementById('txt3'),
-   
- seconds = 0, minutes = 0, hours = 0, t;
-    start.textContent = "00:00:00";
-
-
-
  var piecePuzzle;
-
- var drag=false;
  var mouseX;
  var mouseY;
  var currentPiece;
  var currentDropPiece;
  
- var mx, my;
+ var dd,mm,yyyy;
+ var x,y ;
+
+ var touchSupported=false;
  
- function init()
- {
+ var start, seconds, minutes, hours, t;
+ 	
+ start = document.getElementById('txt3'),
+   
+ seconds = 0, minutes = 0, hours = 0, t;
+ start.textContent = "00:00:00";
+
+ function init(){	 
+	mouse = {x:0,y:0};
+	img = new Image();
+	img.addEventListener('load',onImage,false);
+	img.src = "images/Puzzle4.JPG";
 	 
-	 mouse = {x:0,y:0};
-	 img = new Image();
-	 img.addEventListener('load',onImage,false);
-	 img.src = "images/Puzzle4.JPG";
-	 
-	  img2=new Image();
-	  img2.addEventListener('load',onImage,false);
-	 img2.src="images/start.jpg";
-	 
-	 
+	img2=new Image();
+	img2.addEventListener('load',onImage,false);
+	img2.src="images/start.jpg"; 
  }
  
  function pokaz(){
-	  //var pokazobrazek = document.getElementById("znikajacyobrazek");
-	  document.getElementById("page").style.textAlign = "left"; 
-	  document.getElementById("znikajacyobrazek").style.display = "block"; 
+	document.getElementById("page").style.textAlign = "left"; 
+	document.getElementById("znikajacyobrazek").style.display = "block"; 
  
-	 setTimeout(function(){ document.getElementById("znikajacyobrazek").style.display = "none"; document.getElementById("page").style.textAlign = "center"; },5000);
-
+	setTimeout(function(){ document.getElementById("znikajacyobrazek").style.display = "none"; document.getElementById("page").style.textAlign = "center"; },5000);
  }
 
 function startTime() {
@@ -74,22 +61,17 @@ function checkTime(i) {
 }
 
 function dateTime() {
-		var today = new Date();
-dd = String(today.getDate()).padStart(2, '0');
-mm = String(today.getMonth() + 1).padStart(2, '0');
-yyyy = today.getFullYear();
+	var today = new Date();
+	dd = String(today.getDate()).padStart(2, '0');
+	mm = String(today.getMonth() + 1).padStart(2, '0');
+	yyyy = today.getFullYear();
 
-today = dd + '.' + mm + '.' + yyyy;
+	today = dd + '.' + mm + '.' + yyyy;
 
-document.getElementById('txt2').innerHTML =
-  today;
-  
-
- 
+	document.getElementById('txt2').innerHTML = today;
 }
 
 function add() {
-	
     seconds++;
     if (seconds >= 60) {
         seconds = 0;
@@ -105,14 +87,10 @@ function add() {
     timer();
 }
 function timer() {
-	
-	
     t = setTimeout(add, 1000);
 }
 
-
- function onImage()
- {
+ function onImage(){
 	pieceWidth = Math.floor(img.width / puzzle_level);
     pieceHeight = Math.floor(img.height / puzzle_level);
     
@@ -124,55 +102,32 @@ function timer() {
     initPuzzle();
 }
 
-function setCanvas()
-{
+function setCanvas(){
     canvas.width = puzzleWidth;
     canvas.height = puzzleHeight+38;
-    canvas.style.border = "1px solid black";
-	
-	
+    canvas.style.border = "1px solid black";		
 }
 
-
-function initPuzzle()
-{
+function initPuzzle(){
 	currentPiece = null;
     currentDropPiece = null
-	 ctx.drawImage(img, 0, 0, puzzleWidth, puzzleHeight, 0, 0, puzzleWidth, puzzleHeight);
+	ctx.drawImage(img, 0, 0, puzzleWidth, puzzleHeight, 0, 0, puzzleWidth, puzzleHeight);
 
-	 ctx.drawImage(img2, 200, 510);
-if (Modernizr.touch) {
-
+	ctx.drawImage(img2, 200, 510);
+	
+	if (Modernizr.touch) {
         touchSupported = true;
-
-        // eventsMap  = {
-
-            // select: "touchstart",
-
-            // down: "touchstart",
-
-            // up: "touchend",
-
-            // move: "touchmove"
-
-        // };
-
     }
 	buildPieces();
-
 }
-
-
 	
-function buildPieces()
-{
+function buildPieces(){
 	pieces=[];
 	var piece;
 	var wx=0;
 	var wy=0;
 	
-	for(var i=0; i<puzzle_level*puzzle_level;i++)
-	{
+	for(var i=0; i<puzzle_level*puzzle_level;i++){
 		piece={};
 		piece.x=wx;
 		piece.y=wy;
@@ -184,8 +139,7 @@ function buildPieces()
 		pieces.push(piece);
 		
 		wx+=pieceWidth;
-		
-		
+				
 		if(wx>=puzzleWidth)
 		{
 			wx=0;
@@ -193,63 +147,47 @@ function buildPieces()
 		}								
 	}		
 	document.onmousedown=shufflePuzzle;
-
-	
 }
 
-
-
 function shufflePuzzle(e){
-	
+    var piece;
+    var wx = 0;
+    var wy = 0;
 	var bRect = canvas.getBoundingClientRect();
 	mouseX = (e.clientX - bRect.left);
 	mouseY = (e.clientY - bRect.top);
 	console.log(mouseY);
+	
 	if(mouseX>201 && mouseX<299 && mouseY>510 && mouseY<546)
 	{
 		timer();
-	 //document.getElementById("start").style.display = "none"; 
-			canvas.height=puzzleHeight;
-			pieces = shuffleArray(pieces);
+		canvas.height=puzzleHeight;
+		pieces = shuffleArray(pieces);
 
-    var i;
-    var piece;
-    var wx = 0;
-    var wy = 0;
-    for(i = 0;i < pieces.length;i++){
-        piece = pieces[i];
-        piece.xx = wx;
-        piece.yy = wy;
-		piece.width=pieceWidth;
-		piece.height=pieceHeight;
-        ctx.drawImage(img, piece.x, piece.y, pieceWidth, pieceHeight, wx, wy, pieceWidth, pieceHeight);
-        ctx.strokeRect(wx, wy, pieceWidth,pieceHeight);
-        wx += pieceWidth;
-        if(wx >= puzzleWidth){
-            wx = 0;
-            wy += pieceHeight;
-        }
-    }
-    if(!touchSupported)
-	{
-		 document.onmousedown = mouseDown;
-	}
-	else
-	{
-		
-		//img.addEventListener('load',onImage,false);
-		document.ontouchstart = mouseDown;
+		for(var i = 0;i < pieces.length;i++){
+			piece = pieces[i];
+			piece.xx = wx;
+			piece.yy = wy;
+			piece.width=pieceWidth;
+			piece.height=pieceHeight;
+			ctx.drawImage(img, piece.x, piece.y, pieceWidth, pieceHeight, wx, wy, pieceWidth, pieceHeight);
+			ctx.strokeRect(wx, wy, pieceWidth,pieceHeight);
+			wx += pieceWidth;
 			
-			//mouseDown();
-		//};
-	}
-	}
-
-	
-
-    
+			if(wx >= puzzleWidth){
+				wx = 0;
+				wy += pieceHeight;
+			}
+		}
+		
+		if(!touchSupported){
+		 document.onmousedown = mouseDown;
+		}
+		else{
+		document.ontouchstart = mouseDown;
+		}
+	}  
 }
-
 	
 function shuffleArray(o)
 {
@@ -258,8 +196,7 @@ function shuffleArray(o)
 		const j = Math.floor(Math.random()*i);
 		const temp = o[i];
 		o[i]=pieces[j];
-		o[j]=temp;
-	
+		o[j]=temp;	
 	}
     return o;
 }
@@ -269,100 +206,72 @@ function inPiece()
 	var piece;
 	
 	for(var i=0; i<pieces.length;i++)
-	{
-		
+	{	
 		piece=pieces[i];
 		if(mouseX<piece.xx || mouseX>(piece.xx+pieceWidth)||mouseY<piece.yy || mouseY>(piece.yy+pieceHeight))
-		{
-		
-		}
-		else
-		{
+		{}
+		else{
 			return piece;
-		}
-		
+		}		
 	}
 	return null
 }
 
-function mouseDown(e)
-{
-
+function mouseDown(e){
 	var bRect = canvas.getBoundingClientRect();
-	if(!Modernizr.touch)
-	{
+	if(!Modernizr.touch){
 		mouseX = (e.clientX - bRect.left);
-	mouseY = (e.clientY - bRect.top);
+		mouseY = (e.clientY - bRect.top);
 	}
-	else
-	{
-	
+	else{	
 		mouseX = (e.touches[0].clientX - bRect.left);
-	mouseY = (e.touches[0].clientY - bRect.top);
+		mouseY = (e.touches[0].clientY - bRect.top);
 	}
 	
 	currentPiece=inPiece();
 	
-	if(currentPiece!=null)
-	{
+	if(currentPiece!=null){
 		ctx.clearRect(currentPiece.xx,currentPiece.yy,pieceWidth,pieceHeight);
         ctx.save();
         ctx.globalAlpha = .9;
         ctx.drawImage(img, currentPiece.x, currentPiece.y, pieceWidth, pieceHeight, mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth, pieceHeight);
         ctx.strokeRect(currentPiece.x, currentPiece.y, pieceWidth,pieceHeight);
 		ctx.restore();
-        if(!touchSupported)
-		{
-			
+        if(!touchSupported){	
 			document.onmousemove = mouseMove;
-        document.onmouseup = mouseUp;
+			document.onmouseup = mouseUp;
 		}
-		else
-		{
-			
+		else{			
 			canvas.addEventListener('touchmove', mouseMove, {passive: false});
 			e.stopPropagation();
-			//document.ontouchmove =mouseMove;
-			//document.ontouchend = mouseUp;
 			document.ontouchend = mouseUp;
 		}
-	}
-	
+	}	
 }
 
-
-
-
-function mouseMove(e)
-{
+function mouseMove(e){
 	currentDropPiece = null;
 	
-e.preventDefault();
+	e.preventDefault();
 	e.stopPropagation();
 
 	var bRect = canvas.getBoundingClientRect();
 	
-	if(!Modernizr.touch)
-	{
+	if(!Modernizr.touch){
 		mouseX = (e.clientX - bRect.left);
-	mouseY = (e.clientY - bRect.top);
+		mouseY = (e.clientY - bRect.top);
 	}
-	else
-	{
-	
+	else{	
 		mouseX = (e.touches[0].clientX - bRect.left);
-	mouseY = (e.touches[0].clientY - bRect.top);
+		mouseY = (e.touches[0].clientY - bRect.top);
 	}
 	
 	ctx.clearRect(0,0,puzzleWidth,puzzleHeight);
 	
 	var piece;
-	for(var i=0; i<pieces.length;i++)
-	{
-		
+	for(var i=0; i<pieces.length;i++){	
 		piece=pieces[i];
-		if(piece==currentPiece)
-		{
+		if(piece==currentPiece){
 			continue;
 		}
 		
@@ -378,38 +287,20 @@ e.preventDefault();
                 ctx.fillRect(currentDropPiece.xx,currentDropPiece.yy,pieceWidth, pieceHeight);
                 ctx.restore();
             }
-        }
-			
+        }		
 	}
 	ctx.save();
     ctx.globalAlpha = .6;
     ctx.drawImage(img, currentPiece.x, currentPiece.y, pieceWidth, pieceHeight, mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth, pieceHeight);
     ctx.strokeRect(currentPiece.x, currentPiece.y, pieceWidth,pieceHeight);
 	ctx.restore();
-    ctx.strokeRect( mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth,pieceHeight);
-	
+    ctx.strokeRect( mouseX - (pieceWidth / 2), mouseY - (pieceHeight / 2), pieceWidth,pieceHeight);	
 }
 
-
-
-function mouseUp()
-{
-	if(!touchSupported)
-	{
-			
-	document.onmousemove = null;
-    document.onmouseup = null;
-	}
-	else
-	{
-		//document.ontouchmove = null;
-		
-				
-				//document.ontouchend = null;
-		
-		
-		
-		//canvas.unbind('touchend');
+function mouseUp(){
+	if(!touchSupported){			
+		document.onmousemove = null;
+		document.onmouseup = null;
 	}
 
     if(currentDropPiece != null){
@@ -433,44 +324,35 @@ function resetPuzzleAndCheckWin(){
 	
         ctx.drawImage(img, piece.x, piece.y, pieceWidth, pieceHeight, piece.xx, piece.yy, pieceWidth, pieceHeight);
         ctx.strokeRect(piece.xx, piece.yy, pieceWidth,pieceHeight);
-       if(piece.xx != piece.x || piece.yy != piece.y){
-            gameWin = false;
-			
-        }
-		
+		if(piece.xx != piece.x || piece.yy != piece.y){
+            gameWin = false;	
+        }	
     }
 	
-	if(gameWin)
-	{
+	if(gameWin){
 		setTimeout(gameOver,500);
-	}
-    
+	}  
 }
 
 function myStopFunction() {
-              clearInterval(t);
-      }
+    clearInterval(t);
+}
  
-
 function gameOver(){
     document.onmousedown = null;
     document.onmousemove = null;
     document.onmouseup = null;
 	
 	var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =
-  h + ":" + m + ":" + s;
-
-	
-              
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('txt').innerHTML =
+	h + ":" + m + ":" + s;       
    
 	alert("Gratulacje! \nUkończyłaś/eś poziom trudny w czasie: "+start.textContent+"\nData: " +dd+"."+mm+"."+yyyy + "\nGodzina zakończenia układania puzzli: "+ h+":"+m+":"+s  +"\nTwoje hasło to: ********");
   
 	myStopFunction();
-	
 }
